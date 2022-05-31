061c5fdcb0cd87b35cf0036df1fd70ebbeee107d
Janainamalves <janainamendonca2202@gmail.com>
2022-05-30 22:43:45 -0300

    commit inicial

---
+++ src/ContaCorrente.java
@@ -0,0 +1,10 @@
+
+public class ContaCorrente extends Conta {
+	
+	private static int SEQUENCIAL = 1;
+
+	public ContaCorrente() {
+		super.agencia = AGENCIA_PADRAO;
+		super.numero = SEQUENCIAL++;
+	}
+}
---
+++ src/Main.java
@@ -0,0 +1,11 @@
+
+public class Main {
+
+	public static void main(String[] args) {
+		new ContaCorrente();
+		new ContaPoupanca();
+		
+		System.out.println();
+	}
+
+}
